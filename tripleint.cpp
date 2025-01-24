71ea944c2a6af3286f5c98707ce1b83c76c1282a
Hailey Arnold <0989987@my.scccd.edu>
2025-01-23 16:24:26 -0800

    Add files via upload

---
+++ tripleint.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to my triple integer assignment!" << std::endl;
+
+    int x;
+
+    std::cout << "Type a number: ";
+    std::cin >> x; // get your input number
+
+    int y = x * 3;
+
+    std::cout << "Your number is: " << y; // Display the input value
+
+    return 0;
+}
