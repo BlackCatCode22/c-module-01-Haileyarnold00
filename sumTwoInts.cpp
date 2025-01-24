f3f239917d0bf70b70f7d7233dc70638b9fd92ff
Hailey Arnold <0989987@my.scccd.edu>
2025-01-23 16:30:14 -0800

    Add files via upload

---
+++ sumTwoInts.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Welcome to my sum assignment!" << std::endl;
+
+    int x;
+    int y;
+    int z;
+
+    std::cout << "Type your first number: ";
+    std::cin >> x;
+
+    std::cout << "Enter your second number: ";
+    std::cin >> y;
+
+    z = x + y;
+
+    std::cout << "Your number is: " << z;
+
+
+    return 0;
+}
